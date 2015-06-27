292290d0a7bf7357fe7d3e53a13f4bd15c2c1b6a
damien7851 <damien.pageot78@gmail.com>
2015-06-27 20:02:17 +0200

    implémentation control GPIO pin/val 	modifié:         HAL.c 	modifié:         Init.c 	modifié:         PeripHALpoppy-com.cbp 	modifié:         PeripHALpoppy-com.depend 	modifié:         PeripHALpoppy-com.layout 	modifié:         gdb.conf 	modifié:         include/HAL.h 	modifié:         include/PeriphMaster.h 	modifié:         poppy-com/src/i2c_slave.c 	modifié:         protocol.c

--- HAL.c
+++ HAL.c
@@ -40,8 +40,9 @@
  *  \return nothing
  *
  *  \details Details
- */void setGpioBit(const uchar numgpio,eBool val)
+ */eBool setGpioBit(const uchar numgpio,eBool val)
  {
+ eBool error = false;
    switch(numgpio)
 	 {
 	 case 0 :
@@ -53,13 +54,63 @@
 	 case 2 :
 	 GPIO(2,val); //c'est une macro
 	 break;
+	 case 3 :
+	 GPIO(3,val); //c'est une macro
+	 break;
+	 case 4 :
+	 GPIO(4,val); //c'est une macro
+	 break;
+	 case 5 :
+	 GPIO(5,val); //c'est une macro
+	 break;
+	 case 6 :
+	 GPIO(6,val); //c'est une macro
+	 break;
+	 case 7 :
+	 GPIO(7,val); //c'est une macro
+	 break;
+	 case 8 :
+	 GPIO(8,val); //c'est une macro
+	 break;
+	 case 9 :
+	 GPIO(9,val); //c'est une macro
+	 break;
+	 case 10 :
+	 GPIO(10,val); //c'est une macro
+	 break;
+	 case 11 :
+	 GPIO(11,val); //c'est une macro le premier caractére est substituer et ne peut donc par etre variable
+	 break;
+	 case 12 :
+	 GPIO(12,val); //c'est une macro
+	 break;
+	 case 13 :
+	 GPIO(13,val); //c'est une macro
+	 break;
+	 case 14 :
+	 GPIO(14,val); //c'est une macro
+	 break;
+	 case 15 :
+	 GPIO(15,val); //c'est une macro
+	 break;
+	 case 16 :
+	 GPIO(16,val); //c'est une macro
+	 break;
+	 case 17 :
+	 GPIO(17,val); //c'est une macro
+	 break;
+	 case 18 :
+	 GPIO(18,val); //c'est une macro
+	 break;
+	 case 19 :
+	 GPIO(9,val); //c'est une macro
+	 break;
+
 	 default:
+	 error = true;
 	 break;
 	 }
-	/*if (val==true)
-	setGPIO(numgpio);
-	else
-	resetGPIO(numgpio);*/
+
 
 
  }
